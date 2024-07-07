581869aa9c8f879165dfb19805f3c8da7ffc2087
bedlam343 <jagik22@gmail.com>
2024-07-07 15:48:22 -0700

    justify-evenly navigation header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ const Home = () => {
     <div className="h-screen">
       {/* header */}
       <div className="flex justify-center w-screen">
-        <div className="bg-[#1B1B1B] w-[100%] sm:w-[100%] xl:w-[1280px] rounded-b-lg h-[80px] flex items-center justify-center">
-          <ul className="flex gap-x-12 md:gap-x-16 lg:gap-x-24 xl:gap-x-32 text-xl text-[#9C9C9C]">
+        <div className="bg-[#1B1B1B] w-[100%] sm:w-[100%] lg:w-[1024px] rounded-b-lg h-[60px] flex items-center justify-center">
+          <ul className="flex justify-evenly w-[100%] text-sm text-[#9C9C9C]">
             <li className="hover:text-white hover:cursor-pointer">Home</li>
             <li className="hover:text-white hover:cursor-pointer">
               Experience
